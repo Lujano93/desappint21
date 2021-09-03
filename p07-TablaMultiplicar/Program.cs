859f6e27b94a262612dbddb06153a813b9a0c481
Lujano93 <lujanoGarcia@hotmail.com>
2021-09-03 15:46:42 -0500

    va

--- p07-TablaMultiplicar/Program.cs
+++ p07-TablaMultiplicar/Program.cs
@@ -7,13 +7,15 @@ namespace p07_TablaMultiplicar
         static void Main(string[] args)
         {
             int num=0, i=0, resultado=0;
-            Console.WriteLine("Ingrese el valor a multiplicar: ");
+            Console.WriteLine("Ingrese el valor a multiplicar para la tabla del que ingreses desde 1 al 10: ");
             num = int.Parse(Console.ReadLine());
             for(i=1;i<=10;i++)
             {
                 resultado = num*i;
                 Console.WriteLine(num+" * "+i+"="+resultado);
             }
+
+
         }
     }
 }
