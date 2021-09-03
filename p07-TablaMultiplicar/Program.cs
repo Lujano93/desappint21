a5ad36d95982b98b15824643d6e3d9b562e27c75
Lujano93 <lujanoGarcia@hotmail.com>
2021-09-03 15:38:09 -0500

    va

---
+++ p07-TablaMultiplicar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace p07_TablaMultiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=0, i=0, resultado=0;
+            Console.WriteLine("Ingrese el valor a multiplicar: ");
+            num = int.Parse(Console.ReadLine());
+            for(i=1;i<=10;i++)
+            {
+                resultado = num*i;
+                Console.WriteLine(num+" * "+i+"="+resultado);
+            }
+        }
+    }
+}
