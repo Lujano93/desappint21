c5ce4005882febad69db668a7b76c371abd8e807
Lujano93 <lujanoGarcia@hotmail.com>
2021-09-02 21:25:39 -0500

    va

---
+++ p06-pizza/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static System.Console;
+ 
+
+namespace p06_pizza
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            char tam, cub;
+            string[] ings;
+
+            string tamaño, cubierta;
+            string ingredientes="";
+
+            if(args.Length<3){
+                Menu();
+                return 1;
+            }
+            //Tamaño de la pizza
+            tam = char.Parse(args[0].ToUpper());
+            if(tam=='P') tamaño="Pequeña";
+            else if(tam =='M') tamaño="Mediana";
+            else tamaño="Grande";
+
+            //Ingredientes
+            ings = args[1].Split("+");
+            foreach (String i in ings)
+            {
+                switch (char.Parse(i.ToUpper()))
+                {
+                    
+                    case 'E' : ingredientes += "Extraqueso\n"; break;
+                    
+                    case 'C' : ingredientes += "Champiñones\n"; break;
+                    
+                    case 'J' : ingredientes += "Jalapeño\n"; break;
+                    
+                    case 'T' : ingredientes += "Tocino\n"; break;
+                    
+                    case 'M' : ingredientes += "Jamon\n"; break;
+                }
+            }
+
+            WriteLine("Tu pizza es de tamaño: {0}", tamaño);
+            WriteLine("Tus Ingredientes:\n{0}", ingredientes);
+            return 0;
+
+        }
+        static void Menu(){
+            Clear();
+            WriteLine("Tamaños: (P) - Pequeña \n(M) - Mediana \n(G) - GRande");
+            WriteLine("Estos son los Ingredientes:\n(C) - Champiñones \n(E) - Extra queso \n(J) - Jalapeño \n(T) - Tocino \n(M) - Jamon");
+
+        }
+    }
+}
