c01d31809204be066397cc8474854c6648904787
Lujano93 <lujanoGarcia@hotmail.com>
2021-09-04 23:39:07 -0500

    va

---
+++ p10-VectoCubo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p10_VectoCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
