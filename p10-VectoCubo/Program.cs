4704f78fa71ca3ccdd4ad546dd6a43bc6a410283
Lujano93 <lujanoGarcia@hotmail.com>
2021-09-04 23:53:15 -0500

    va

--- p10-VectoCubo/Program.cs
+++ p10-VectoCubo/Program.cs
@@ -6,7 +6,26 @@ namespace p10_VectoCubo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int TAM=20;
+            double[] A = new double[TAM];
+            double[] B = new double[TAM];
+            Random rnd = new Random();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rnd.Next(1,200);
+                B[i] = Math.Pow(A[i],3);
+            }
+            Console.WriteLine("\nElementos de A: "); imprime(A);
+            Console.WriteLine("\nElementos de B: "); imprime(B);
+        }
+        static void imprime(double[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+            Console.WriteLine();
         }
     }
 }
