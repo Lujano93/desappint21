3718747799d60b59d5347aa3877f6b3d93562a7e
Lujano93 <lujanoGarcia@hotmail.com>
2021-09-06 15:28:16 -0500

    va

---
+++ p12-VectorInvertido/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace p12_VectorInvertido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAM=16;
+            int[] A = new int[TAM];
+            int[] B = new int[TAM];
+            Random rnd = new Random();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rnd.Next(1,100);
+                B[15-i] = A[i];
+            }
+            Console.WriteLine("\n Valores de A: "); imprime(A);
+            Console.WriteLine("\n Valores de B: "); imprime(B);
+        }
+        static void imprime(int[] v){
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ p14-Estadisticas/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace p14_Estadisticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double mayor, menor, promedio, varianza, desviacion;
+            int n; 
+            Console.Write("Cuantos valores deseas procesar: ");
+            n = int.Parse(Console.ReadLine());
+            double[] v = new double[n];
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine($"n[{i+1}]= ");
+                v[i] = double.Parse(Console.ReadLine());
+            }
+            mayor = may(v);
+            menor = men(v);
+            promedio = prom(v);
+            varianza = var(v, promedio);
+            desviacion = Math.Sqrt(varianza);
+
+            Console.WriteLine($"\nEl Mayor es: {mayor}");
+            Console.WriteLine($"\nEl Menor es: {menor}");
+            Console.WriteLine($"\nEl Promedio es: {promedio}");
+            Console.WriteLine($"\nLa varianza es:  {varianza}");
+            Console.WriteLine($"\nLa desviación estandar es:  {desviacion}");
+        }
+
+        static double var(double[] v, double p){
+            double s=0;
+            for (int i = 0; i < v.Length; i++)
+                s += Math.Pow(v[i] - p,2);
+                return s / v.Length;
+        }
+
+        static double prom(double[] v){
+            double s=0;
+            for (int i = 0; i < v.Length; i++)
+                s+=v[i];
+                return s / v.Length;
+        }
+
+        static double may(double[] v){
+            double m= v[0];
+            for (int i = 0; i < v.Length; i++)
+                if(v[i]>m)
+                m = v[i];
+                return m;
+        }
+
+          static double men(double[] v){
+            double m= v[0];
+            for (int i = 0; i < v.Length; i++)
+                if(v[i]<m)
+                m = v[i];
+                return m;
+        }
+
+    }
+}
